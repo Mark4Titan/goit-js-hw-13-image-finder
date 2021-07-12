b5c01fa7f8fe3553f2c113e0f52a85da579cfda2
Mark <52928930+Mark4Titan@users.noreply.github.com>
2021-07-12 17:24:36 +0300

    переробив дз

--- src/index.js
+++ src/index.js
@@ -1,38 +1,69 @@
-import './styles.css';
-import searchImgFn from './js/apiService';
-import gallery from './templates/gallery.hbs';
+import './sass/main.scss';
+import PixabayApiService from './js/pixabayApi';
+import photosListTpl from './templates/photos.hbs';
+import { appendItemCountryMarkup, cleanContent, createPhotoModal } from './js/markups';
+import { refs } from './js/refs';
+import { photoFetch } from './js/fetchApi';
 
-const debounce = require('lodash.debounce');
-const searchForm = document.querySelector('#search-form');
-const divMark = document.querySelector('.mark');
-const apiKey = '20461350-36527ad634bc0878b1b72e118';
-const firstPage = 1;
-let currentNextPage = 1;
-let searchWords;
+// var debounce = require('lodash.debounce');
 
-searchForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    divMark.innerHTML = '';
-    searchWords = searchForm.elements.query.value;
-    const title = { searchWords };
-    const galleryMarkup = gallery(title);
-    divMark.insertAdjacentHTML('beforeend', galleryMarkup);
-    const btn = document.querySelector('.btn-load-more');
-    btn.addEventListener('click', (event) => {
-        loadMoreFn(event, currentNextPage);  
-    });
-    createFirstPage(searchWords);
-    searchForm.reset();
+const pixabayApiService = new PixabayApiService();
+
+const inputButton = refs.inputBtn;
+// inputButton.addEventListener('input', debounce(onInputSearch, 800));
+
+const searchForm = refs.searchForm;
+searchForm.addEventListener('submit', onInputSearch);
+
+const photoCard = refs.photoCard;
+photoCard.addEventListener('click', onClickPhotoCard);
+
+// LOAD MORE
+const observer = new IntersectionObserver(entries => {
+  const firstEntry = entries[0];
+
+  if (firstEntry.isIntersecting) {
+    if (inputButton.value) {
+      pixabayApiService.page += 1;
+
+      const infoMsg = 'No more images. This is all that was found on your request';
+      const errMsg = 'Invalid query.';
+      photoFetch(pixabayApiService, infoMsg, errMsg);
+    }
+  }
 });
 
-const createFirstPage = (words) => {
-    searchImgFn(words, firstPage, apiKey);}
+const scrollArea = refs.scrollArea;
+observer.observe(scrollArea);
+console.log('observer 2: ', observer);
+// END LOAD MORE
 
-function loadMoreFn(event, page) { 
-    console.log(`click`, event);
-    searchImgFn(searchWords, page, apiKey);
-    currentNextPage = page + 1;
-    console.log(currentNextPage);
-};
+function onInputSearch(e) {
+  e.preventDefault();
+  const form = e.currentTarget;
+  console.log('onInputSearch ~ form: ', form);
 
+  const photoQuery = form.elements.query.value.trim();
+  console.log('onInputSearch ~ photoQuery: ', photoQuery);
 
+  if (photoQuery) {
+    pixabayApiService.page = 1;
+    cleanContent(refs.container);
+
+    pixabayApiService.photo = photoQuery;
+    const infoMsg = 'No more images';
+    const errMsg = 'Invalid query.';
+    photoFetch(pixabayApiService, infoMsg, errMsg);
+  } else {
+    console.log('onInputSearch ~ photoQuery 2: ', photoQuery);
+    appendItemCountryMarkup(refs.container, photosListTpl, '');
+  }
+}
+
+function onClickPhotoCard(e) {
+  const photoEl = e.target;
+  const largeImage = photoEl.dataset.largeImage;
+  const photoAlt = photoEl.getAttribute('alt');
+
+  createPhotoModal(largeImage, photoAlt);
+}
